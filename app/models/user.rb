a80ddff653324f189ea217b598086b1c306b4a3f
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-06-03 12:05:25 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :articles
   has_many :likes
+  has_many :follows
   validates :account_id, :account_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
