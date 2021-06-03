9bc3be9d1280257c9c89d260237b6c3f3e41aaf6
ryo5515 <ryo.miyawaki5515@gmail.com>
2021-06-03 12:17:42 +0900

    JavaScriptの編集

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -13,5 +13,29 @@ document.addEventListener("turbolinks:load", function(){
         }
       })
     })
+    $("#follow").on("click", function(){
+      $.ajax({
+        url: $(this).attr("action"),
+        type: "get"
+      }).done(function(json){
+        if(json.follow == "follow"){
+          $("#follow").text("フォロー中");
+          $("#follower").text(`フォロワー数:${json.follower}`);
+          json.followers.forEach(function(follower){
+            $("#follower").append(`
+              <a href="${follower.link}">${follower.name}</a>さん 
+            `);
+          })
+        }else{
+          $("#follow").text("フォローする");
+          $("#follower").text(`フォロワー数:${json.follower}`);
+          json.followers.forEach(function(follower){
+            $("#follower").append(`
+              <a href="${follower.link}">${follower.name}</a>さん 
+            `);
+          })
+        }
+      })
+    })
   })
 })
